50956163b60b7a7b41026a6ead30e0b913840c20
mszawerd <01169246@pw.edu.pl>
2023-05-05 11:47:31 +0200

    Added changing state of poll

--- components/PollCard.tsx
+++ components/PollCard.tsx
@@ -1,31 +1,6 @@
 import {SupaResponse} from "../lib/Types";
-
-// const data = {
-//     question: "Neque porro quisquam est qui dolorem consectetur?",
-//     shortId: "abc123",
-//     answers: {
-//         "1": {
-//             id: 0,
-//             text: "Lorem ipsum dolor sit amet, consectetur1",
-//             count: 2
-//         },
-//         "2": {
-//             id: 1,
-//             text: "Lorem ipsum dolor sit amet, consectetur2",
-//             count: 3
-//         },
-//         "3": {
-//             id: 2,
-//             text: "Lorem ipsum dolor sit amet, consectetur3",
-//             count: 2
-//         },
-//         "4": {
-//             id: 3,
-//             text: "Lorem ipsum dolor sit amet, consectetur4",
-//             count: 10
-//         },
-//     }
-// }as SupaResponse
+import {useState} from "react";
+import {supaClient} from "../lib/supa-client";
 
 interface PollCardProps{
     onOpen: ()=>void
@@ -33,11 +8,26 @@ interface PollCardProps{
 }
 
 export const PollCard = ({onOpen, data}:PollCardProps) => {
-    const answers = Object.values(data.answers)
-    const color = data.isopened ? "green" : "red"
-    const state = data.isopened ? "Opened" : "Finished"
+    const [pollData, setPollData ] = useState(data)
+    const answers = Object.values(pollData.answers)
+    const color = pollData.isopened ? "green" : "red"
+    const state = pollData.isopened ? "Opened" : "Finished"
+    console.log("id", pollData.id)
+    const onFinishPoll = () => {
+        const finishPoll = async () => {
+            const {data, error} = await supaClient
+                .from("polls")
+                .update({isopened: false})
+                .match({id: pollData.id})
+                .select()
+            if(error) console.error(error)
+            else setPollData(data[0] as SupaResponse)
+        }
+        finishPoll().then()
+    }
+
     return <div className="poll-card" >
-        <div className="poll-card-heading">Question: {data.question}</div>
+        <div className="poll-card-heading">Question: {pollData.question}</div>
         <div className="poll-card-state"><b>State: </b> <span style={{color: color}}>{state}</span></div>
         <div className="poll-card-answers">
             {
@@ -56,8 +46,8 @@ export const PollCard = ({onOpen, data}:PollCardProps) => {
         <div className="poll-card-buttons poll-card-answers">
             <div className="poll-card-button" onClick={onOpen}>Open</div>
             <div className="poll-card-button">To pdf</div>
-            {data.isopened ?
-                <div className="poll-card-button">Finish</div> : <></>
+            {pollData.isopened ?
+                <div className="poll-card-button" onClick={onFinishPoll}>Finish</div> : <></>
             }
             <div className="poll-card-button">Delete</div>
         </div>
