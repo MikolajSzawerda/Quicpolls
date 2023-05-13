7ba352cb520e30ec4d5ca2e2210907ef7b1987ed
mszawerd <01169246@pw.edu.pl>
2023-05-13 23:54:01 +0200

    Add answer to enter

--- components/AnswerEdit.tsx
+++ components/AnswerEdit.tsx
@@ -1,5 +1,5 @@
 import {AnswerButton} from "./AnswerButton";
-import {useState} from "react";
+import {SyntheticEvent, useState} from "react";
 import DeleteSVG from "../src/assets/delete.svg";
 import EditSVG from "../src/assets/edit.svg";
 
@@ -21,6 +21,18 @@ export const AnswerEdit = ({text, idx, onEdit, onDelete}: AnswerEditProps) => {
         setIsHover(false)
     }
 
+    const onEnter = (event: SyntheticEvent)=>{
+
+        if((event.nativeEvent as KeyboardEvent)?.key=="Enter"){
+            const value = (event.target as HTMLInputElement).value
+            if (value) {
+                onEdit(value, idx)
+            }
+            setEditing(false)
+            setIsHover(false)
+        }
+    }
+
     const onEditClick = () => {
         setIsHover(false)
         setEditing(true)
@@ -36,7 +48,7 @@ export const AnswerEdit = ({text, idx, onEdit, onDelete}: AnswerEditProps) => {
         editing ?
             <div>
                 <input ref={(input) => input && input.focus()} onBlur={onSubmit} type="text"
-                       className="answer-adder-input"/>
+                       className="answer-adder-input" onKeyDown={onEnter} defaultValue={text}/>
             </div> :
             <div key={idx} className="answer-edit-button" onMouseLeave={() => setIsHover(false)}>
                 <img src={DeleteSVG} className="icon icon-hover" alt="Delete" onClick={onDeleteClick}/>
